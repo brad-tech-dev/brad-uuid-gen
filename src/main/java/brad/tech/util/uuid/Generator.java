bd0dd0e1920add5765ccff0475be477b705aaeb1
BRAD Technologies <bradtech.ke@gmail.com>
2020-05-28 20:46:12 +0300

    Implemented display of the ui in Generator.java

--- src/main/java/brad/tech/util/uuid/Generator.java
+++ src/main/java/brad/tech/util/uuid/Generator.java
@@ -1,5 +1,13 @@
 package brad.tech.util.uuid;
 
+import java.awt.EventQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  * This is the entry-point class.
  * 
@@ -8,7 +16,47 @@ package brad.tech.util.uuid;
  */
 public class Generator {
     
+    private final UIPanel panel;
+    Generator() {
+        panel = new UIPanel();
+    }
+    
+    void display() {
+        final var dialog = new JFrame("BRAD UUID Generator");
+        dialog.setContentPane(panel);
+        
+        // fetch logo
+        final var logoURL = Generator.class.getResource("processor.png");
+        if (logoURL != null) {
+            panel.lblLogo.setIcon(new ImageIcon(logoURL));
+        }
+        
+        // icon
+        final var iconURL = Generator.class.getResource("brad-logo.png");
+        if (iconURL != null) {
+            dialog.setIconImage(new ImageIcon(iconURL).getImage());
+        }
+        
+        dialog.setAlwaysOnTop(true);
+        dialog.setResizable(false);
+        dialog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        dialog.pack();
+        dialog.setLocationRelativeTo(null);
+        dialog.setVisible(true);
+    }
+    
     public static void main(String[] args) {
-        System.out.println("INFO: Starting...");
+        // look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | 
+                InstantiationException | 
+                IllegalAccessException | 
+                UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Generator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // run app
+        EventQueue.invokeLater(new Generator()::display);
     }
 }
