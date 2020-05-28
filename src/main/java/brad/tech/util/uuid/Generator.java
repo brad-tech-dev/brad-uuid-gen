0fb637972dc5c609d02054936276765beeeecea3
BRAD Technologies <bradtech.ke@gmail.com>
2020-05-28 18:06:54 +0300

    Created Generator.java

---
+++ src/main/java/brad/tech/util/uuid/Generator.java
@@ -0,0 +1,14 @@
+package brad.tech.util.uuid;
+
+/**
+ * This is the entry-point class.
+ * 
+ * Contains the main() method.
+ * @author brad
+ */
+public class Generator {
+    
+    public static void main(String[] args) {
+        System.out.println("INFO: Starting...");
+    }
+}
